0d9808327f3fc0f10eeba2a50e5cd94113ce7df4
Aivaras <AIAT@danskebank.lt>
2020-05-23 12:23:54 +0300

    initial project commit

---
+++ MSTD-Backend/MSTD-Backend/Controllers/SourcesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MSTD_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SourcesController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
